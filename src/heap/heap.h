5f62b0813d8796925c5b8a45bb55ff8e45a2a6b6
Filipe Calasans <filipe.calasans@gmail.com>
2018-04-01 13:41:50 -0400

    start heap implementation

---
+++ src/heap/heap.h
@@ -0,0 +1,120 @@
+#ifndef __HEAP_HH__   
+#define __HEAP_HH__ 
+
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+#include <cstdint>
+#include <memory>
+#include <algorithm>
+
+#include "global/assert.h"
+#include "list/list.h"
+
+/* 
+ * Algos Implementation is implicit-sharing.
+ *
+ */
+namespace Algos { 
+
+template <class K, class T>
+class Heap : protected List<T> {
+  
+  private:
+
+    int heap_order = 2;
+
+  public: 
+    
+    Heap(int order = 2) { 
+      heap_order = order;
+    }
+
+    Heap(const Heap<K,T>& other) : List<T>(other) {
+      heap_order = other.heap_order;
+    }
+
+    void swap(Heap<K,T>& other) {
+      std::swap(this->d, other.d);
+      std::swap<int>(heap_order, other.heap_order);
+    }
+
+    
+    Heap<K,T> &operator=(const Heap<K,T>& other) { 
+      List<T>::operator=(other); 
+      return (*this); 
+    }
+
+    Heap<K,T> &operator=(Heap<K,T>&& other) { 
+      List<T>::operator=(other); 
+      heap_order = other.heap_order;
+      return (*this); 
+    }
+
+    bool operator==(const Heap<K,T>& other) const { return (this->d==other.d) && 
+      (heap_order==other.heap_order); }
+    inline bool operator!=(const Heap<K,T> &other) const { return (this->d != other.d) || 
+      (heap_order!=other.heap_order); }
+    inline int size() const { return List<T>::size(); }
+    inline bool isEmpty() const { return List<T>::size(); }
+    inline void reserve(int size) { List<T>::reserve(size); }
+
+
+    T& operator[](int i) { 
+      return List<T>::operator[](i);
+    }
+
+    const T& operator[](int i) const { 
+      return List<T>::operator[](i);
+    }
+    
+    typename List<T>::iterator begin() { return List<T>::begin(); }
+    typename List<T>::iterator end() { return List<T>::end(); }
+
+    typename List<T>::const_iterator cbegin() const { return List<T>::cbegin(); }
+    typename List<T>::const_iterator cend() const { return List<T>::cend(); }
+    
+    void insert(const K& k, const T& t);
+    void remove(const K& k);
+    bool contains(const K& k);
+    const T& top() const;
+    T peak();
+    
+protected:
+
+    void upheap(int i);
+    int search(const K& k);
+
+
+};
+
+template <class K, class T>
+void Heap<K,T>::insert(const K& k, const T& t) {
+   
+}
+
+template <class K, class T>
+void Heap<K,T>::remove(const K& k) {
+
+}
+
+template <class K, class T>
+bool Heap<K,T>::contains(const K& k) {
+
+}
+
+template <class K, class T>
+const T& Heap<K,T>::top() const {
+
+}
+
+template <class K, class T>
+T Heap<K,T>::peak() {
+
+}
+
+
+}
+
+#endif
---
+++ unit_test/heap/testheap.cpp
@@ -0,0 +1,112 @@
+
+#include <iostream>
+
+#include "heap/heap.h"
+#include "gtest/gtest.h"
+
+#include <string>
+
+using namespace Algos;
+
+struct DataTest {
+   int integer;
+   bool boolean;
+   std::string txt;
+  
+   DataTest() {}
+
+   DataTest(const DataTest& other) {
+    *this = other;
+   }
+
+   DataTest& operator=(const DataTest& other) {
+      integer = other.integer;
+      boolean = other.boolean;
+      txt = other.txt;
+      return (*this);
+   }
+
+   bool operator==(const DataTest& other) const {
+      return (
+         integer == other.integer &&
+         boolean == other.boolean &&
+         txt == other.txt
+      );
+   }
+};
+
+#define TEST_SIZE 100
+
+void initDataTest(DataTest *t, int n) {
+  for(int i=0; i<n; i++) {
+    t[i].integer = i;
+    t[i].boolean = (i%2 == 0);
+    t[i].txt = "abc";
+  }
+}
+
+// Tests the default c'tor.
+TEST(Heap, constructor) {
+   Heap<int,DataTest> l;
+
+   EXPECT_EQ(l.size(), 0);
+   EXPECT_EQ(l.isEmpty(), true);
+}
+
+TEST(Heap, insert) {
+  
+  Heap<int,DataTest> l;
+  DataTest d[TEST_SIZE];
+  
+  initDataTest(d, TEST_SIZE);
+  
+    
+  EXPECT_EQ(TEST_SIZE, l.size());
+  EXPECT_EQ(l.isEmpty(), false);
+
+  int count = 0;
+  for(const auto &i : l) {
+    EXPECT_EQ(i == d[count++], true);
+  }
+
+}
+
+
+TEST(Heap, pull_peak) {
+  
+  Heap<int,DataTest> l;
+  DataTest d[TEST_SIZE];
+  
+  initDataTest(d, TEST_SIZE);
+  
+
+}
+
+
+
+TEST(Heap, swap) {
+  
+  Heap<int,DataTest> l1, l2; 
+  DataTest d1[TEST_SIZE];
+  DataTest d2[TEST_SIZE];
+  
+  initDataTest(d1, TEST_SIZE);
+  initDataTest(d2, TEST_SIZE/2);
+  
+
+  EXPECT_EQ(TEST_SIZE, l1.size());
+  EXPECT_EQ(TEST_SIZE, l2.size());
+  EXPECT_EQ(l1.isEmpty(), false);
+  EXPECT_EQ(l2.isEmpty(), false);
+  
+  l1.swap(l2);
+  
+  int idx=0;
+  for(const auto& i : l1)
+      EXPECT_EQ(i == d2[idx++], true);
+
+  idx=0;
+  for(const auto& i : l2)
+      EXPECT_EQ(i == d1[idx++], true);
+
+}
